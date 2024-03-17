b507505cdbfef7ab4daaa9ce3f053e4a7b979590
IsaevaMariya <162918840+IsaevaMariya@users.noreply.github.com>
2024-03-17 18:29:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # main.py
+import json
+import psutil
+from platform import uname
 
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
@@ -12,4 +15,64 @@ def index():
 @main.route('/profile')
 @login_required
 def profile():
-    return render_template('profile.html', name=current_user.name)
+    info=makelistdisk()
+    return render_template('profile.html', name=current_user.name,infodisk=info)
+#    return render_template('profile.html', name=current_user.name,infodisk=info)
+
+def correct_size(bts, ending='iB'):
+    size = 1024
+    for item in ["", "K", "M", "G", "T", "P"]:
+        if bts < size:
+            return f"{bts:.2f}{item}{ending}"
+        bts /= size
+
+def creating_file():
+    collect_info_dict = dict()
+    if 'info' not in collect_info_dict:
+        collect_info_dict['info'] = dict()
+        collect_info_dict['info']['system_info'] = dict()
+
+
+#Диски
+    for partition in psutil.disk_partitions():
+        try:
+            partition_usage = psutil.disk_usage(partition.mountpoint)
+        except PermissionError:
+            continue
+        if 'disk_info' not in collect_info_dict['info']:
+            collect_info_dict['info']['disk_info'] = dict()
+        if f"'device': {partition.device}" not in collect_info_dict['info']['disk_info']:
+            collect_info_dict['info']['disk_info'][partition.device] = dict()
+            collect_info_dict['info']['disk_info'][partition.device] = {'file_system': partition.fstype,
+                                                                        'size_total': correct_size(
+                                                                            partition_usage.total),
+                                                                        'size_used': correct_size(
+                                                                            partition_usage.used),
+                                                                        'size_free': correct_size(
+                                                                            partition_usage.free),
+                                                                        'percent':
+                                                                            f'{partition_usage.percent}'}
+
+
+    return collect_info_dict
+
+
+def print_info(dict_info):
+    for item in dict_info['info']:
+
+        if item == "disk_info":
+            for elem in dict_info['info'][item]:
+                print(f"[+] Информация о дисках\n"
+                      f"\t- Имя диска: {elem}\n"
+                      f"\t- Файловая система: {dict_info['info'][item][elem]['file_system']}\n"
+                      f"\t- Объем диска: {dict_info['info'][item][elem]['size_total']}\n"
+                      f"\t- Занято: {dict_info['info'][item][elem]['size_used']}\n"
+                      f"\t- Свободно: {dict_info['info'][item][elem]['size_free']}\n"
+                      f"\t- Заполненность: {dict_info['info'][item][elem]['percent']}%\n")
+
+def makelistdisk():
+
+        dict_info = creating_file()
+
+        return print_info(dict_info)
+
